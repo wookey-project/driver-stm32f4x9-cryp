be2e7d5c288a67a49dffabdbaf5fe5dbb29a3e60
Philippe Thierry <phil@reseau-libre.net>
2018-09-18 14:59:35 +0200

    Initial commit

---
+++ api/libcryp.h
@@ -0,0 +1,214 @@
+#ifndef CRYP_H
+#define CRYP_H
+
+#include "api/types.h"
+
+#define CRYP_BASE			0x50060000
+
+#define r_CORTEX_M_CRYP_CR		REG_ADDR(CRYP_BASE + 0x00)
+#define r_CORTEX_M_CRYP_SR		REG_ADDR(CRYP_BASE + 0x04)
+#define r_CORTEX_M_CRYP_DIN		REG_ADDR(CRYP_BASE + 0x08)
+#define r_CORTEX_M_CRYP_DOUT		REG_ADDR(CRYP_BASE + 0x0c)
+#define r_CORTEX_M_CRYP_DMACR		REG_ADDR(CRYP_BASE + 0x10)
+#define r_CORTEX_M_CRYP_IMSCR		REG_ADDR(CRYP_BASE + 0x14)
+#define r_CORTEX_M_CRYP_RISR		REG_ADDR(CRYP_BASE + 0x18)
+#define r_CORTEX_M_CRYP_MISR		REG_ADDR(CRYP_BASE + 0x1c)
+#define r_CORTEX_M_CRYP_KxLR(n)	REG_ADDR(CRYP_BASE + 0x20 + (n * 8))
+#define r_CORTEX_M_CRYP_KxRR(n)	REG_ADDR(CRYP_BASE + 0x24 + (n * 8))
+#define r_CORTEX_M_CRYP_IVxLR(n)	REG_ADDR(CRYP_BASE + 0x40 + (n * 8))
+#define r_CORTEX_M_CRYP_IVxRR(n)	REG_ADDR(CRYP_BASE + 0x44 + (n * 8))
+
+/* CRYP control register */
+#define CRYP_CR_ALGODIR_Pos	2
+#define CRYP_CR_ALGODIR_Msk	((uint32_t)0x1 << CRYP_CR_ALGODIR_Pos)
+#	define CRYP_CR_ALGODIR_ENCRYPT		0
+#	define CRYP_CR_ALGODIR_DECRYPT		1
+#define CRYP_CR_ALGOMODE_Pos	3
+#define CRYP_CR_ALGOMODE_Msk	((uint32_t)0x7 << CRYP_CR_ALGOMODE_Pos)
+#	define CRYP_CR_ALGOMODE_TDES_ECB	0
+#	define CRYP_CR_ALGOMODE_TDES_CBC	1
+#	define CRYP_CR_ALGOMODE_DES_ECB		2
+#	define CRYP_CR_ALGOMODE_DES_CBC		3
+#	define CRYP_CR_ALGOMODE_AES_ECB		4
+#	define CRYP_CR_ALGOMODE_AES_CBC		5
+#	define CRYP_CR_ALGOMODE_AES_CTR		6
+#	define CRYP_CR_ALGOMODE_AES_KEY_PREPARE	7
+#define CRYP_CR_DATATYPE_Pos	6
+#define CRYP_CR_DATATYPE_Msk	((uint32_t)0x3 << CRYP_CR_DATATYPE_Pos)
+#	define CRYP_CR_DATATYPE_WORDS		0
+#	define CRYP_CR_DATATYPE_HALF_WORDS	1
+#	define CRYP_CR_DATATYPE_BYTES		2
+#	define CRYP_CR_DATATYPE_BITS		3
+#define CRYP_CR_KEYSIZE_Pos	8
+#define CRYP_CR_KEYSIZE_Msk	((uint32_t)0x3 << CRYP_CR_KEYSIZE_Pos)
+#	define CRYP_CR_KEYSIZE_128		0
+#	define CRYP_CR_KEYSIZE_192		1
+#	define CRYP_CR_KEYSIZE_256		2
+#define CRYP_CR_FFLUSH_Pos	14
+#define CRYP_CR_FFLUSH_Msk	((uint32_t)1 << CRYP_CR_FFLUSH_Pos)
+#define CRYP_CR_CRYPEN_Pos	15
+#define CRYP_CR_CRYPEN_Msk	((uint32_t)1 << CRYP_CR_CRYPEN_Pos)
+
+/* CRYP status register */
+#define CRYP_SR_IFEM_Pos	0
+#define CRYP_SR_IFEM_Msk	((uint32_t)1 << CRYP_SR_IFEM_Pos)
+#define CRYP_SR_IFNF_Pos	1
+#define CRYP_SR_IFNF_Msk	((uint32_t)1 << CRYP_SR_IFNF_Pos)
+#define CRYP_SR_OFNE_Pos	2
+#define CRYP_SR_OFNE_Msk	((uint32_t)1 << CRYP_SR_OFNE_Pos)
+#define CRYP_SR_OFFU_Pos	3
+#define CRYP_SR_OFFU_Msk	((uint32_t)1 << CRYP_SR_OFFU_Pos)
+#define CRYP_SR_BUSY_Pos	4
+#define CRYP_SR_BUSY_Msk	((uint32_t)1 << CRYP_SR_BUSY_Pos)
+
+/* CRYP DMA control register */
+#define CRYP_DMACR_DIEN_Pos	0
+#define CRYP_DMACR_DIEN_Msk	((uint32_t)1 << CRYP_DMACR_DIEN_Pos)
+#define CRYP_DMACR_DOEN_Pos	1
+#define CRYP_DMACR_DOEN_Msk	((uint32_t)1 << CRYP_DMACR_DOEN_Pos)
+
+/* CRYP interrupt mask set/clear register */
+#define CRYP_IMSCR_INIM_Pos	0
+#define CRYP_IMSCR_INIM_Msk	((uint32_t)1 << CRYP_IMSCR_INIM_Pos)
+#define CRYP_IMSCR_OUTIM_Pos	1
+#define CRYP_IMSCR_OUTIM_Msk	((uint32_t)1 << CRYP_IMSCR_OUTIM_Pos)
+
+/* CRYP raw interrupt status register */
+#define CRYP_RISR_INRIS_Pos	0
+#define CRYP_RISR_INRIS_Msk	((uint32_t)1 << CRYP_RISR_INRIS_Pos)
+#define CRYP_RISR_OUTRIS_Pos	1
+#define CRYP_RISR_OUTRIS_Msk	((uint32_t)1 << CRYP_RISR_OUTRIS_Pos)
+
+/* CRYP masked interrupt status register */
+#define CRYP_MISR_INMIS_Pos	0
+#define CRYP_MISR_INMIS_Msk	((uint32_t)1 << CRYP_MISR_INMIS_Pos)
+#define CRYP_MISR_OUTMIS_Pos	1
+#define CRYP_MISR_OUTMIS_Msk	((uint32_t)1 << CRYP_MISR_OUTMIS_Pos)
+
+/*
+** Cryp user mode (no key injection authorized) or crypto configurator
+** mode
+*/
+enum crypto_usage {
+  CRYP_USER = 0,
+  CRYP_CFG,
+};
+
+/* Benchmode, with key injection from crypto user toward configurator via
+ * IPC (require configurator to be configured in the same way), or in
+ * prod mode, asking the configurator to inject the key based on the
+ * smartcard content
+ */
+enum crypto_mode {
+    CRYP_BENCHMODE,
+    CRYP_PRODMODE,
+};
+
+enum crypto_key_len {
+    KEY_128,
+    KEY_192,
+    KEY_256
+};
+
+enum crypto_algo {
+    TDES_ECB,
+    TDES_CBC,
+    DES_ECB,
+    DES_CBC,
+    AES_ECB,
+    AES_CBC,
+    AES_CTR,
+    AES_KEY_PREPARE
+};
+
+enum crypto_dir {
+    ENCRYPT,
+    DECRYPT
+};
+
+/**
+ * encrypt_no_dma - Encrypt/Decrypt data without DMA
+ * @data_in: Address of the buffer where this function will read data. The
+ * buffer size is @data_len.
+ * @data_out: Address of the buffer where encrypted/decrypted data will be
+ * written. The buffer size must be at least @data_len.
+ * @data_len: Number of bytes to encrypt/decrypt.
+ *
+ * You should call aes_init before calling this function.
+ *
+ * This function returns only when data is encrypted or decrypted (depending on
+ * the direction given in aes_init).
+ *
+ */
+void soc_encrypt_no_dma(const uint8_t * data_in, uint8_t * data_out,
+                        uint32_t data_len);
+
+/**
+ * encrypt_dma - Encrypt/Decrypt data using DMA
+ * @data_in: Address of the buffer where this function will read data. The
+ * buffer size is @data_len.
+ * @data_out: Address of the buffer where encrypted/decrypted data will be
+ * written. The buffer size must be at least @data_len.
+ * @data_len: Number of bytes to encrypt/decrypt.
+ * @dma_in_complete: Callback called when the IN DMA transfer (@data_in to
+ * CRYP) is finished.
+ * @dma_out_complete: Callback called when the OUT DMA transfer (CRYP to
+ * @data_out) is finished.
+ *
+ * You should call aes_init before calling this function.
+ *
+ * This function returns as soon as the DMA transfer is enabled. You should not
+ * read in @data_out buffer before @transfer_complete is called.
+ *
+ */
+//void soc_encrypt_dma(const uint8_t *data_in, uint8_t *data_out, uint32_t data_len,
+//       void (*dma_in_complete)(void), void (*dma_out_complete)(void));
+
+void cryp_set_key(const uint8_t * key, enum crypto_key_len key_len);
+
+void cryp_set_iv(const uint8_t * iv);
+
+void cryp_enable_dma(void);
+
+void enable_crypt(void);
+
+/**
+ * aes_init - Initialize an encryption/decryption
+ * @key: Address of the key. The buffer that contains the key must have a size
+ * of 128, 192 or 256 bytes depending on the vluae of @key_len.
+ * @key_len: The size of the key.
+ * @iv: Address of the initialization vector. The buffer size must be at least
+ * 128 bytes.
+ * @mode: Algorithm: TDES_ECB, TDES_CBC, DES_ECB, DES_CBC, AES_ECB, AES_CBC or
+ * AES_CTR.
+ * @dir: Direction: ENCRYPT or DECRYPT.
+ *
+ * This function should be called before calling encrypt_dma or encrypt_no_dma.
+ */
+void cryp_init(const uint8_t * key, enum crypto_key_len key_len,
+               const uint8_t * iv, enum crypto_algo mode, enum crypto_dir dir);
+
+/* initialize DMA streams for cryp (not runnable, should be reconf later) */
+void cryp_early_init(bool with_dma,
+                     enum crypto_usage usage,
+                     enum crypto_mode mode,
+                     int * dma_in_desc,
+                     int * dma_out_desc);
+
+/* configure the DMA streams with proper informations (handlers, buffers...) */
+void cryp_init_dma(void *handler_in, void *handler_out, int dma_in_desc,
+                   int dma_out_desc);
+
+/*
+ * start cryp with no DMA support
+ */
+void cryp_do_no_dma(const uint8_t * data_in, uint8_t * data_out,
+                    uint32_t data_len);
+
+/*
+ * start cryp using DMA (this requires libdma)
+ */
+void cryp_do_dma(const uint8_t * bufin, const uint8_t * bufout, uint32_t size,
+                 int dma_in_desc, int dma_out_desc);
+
+#endif                          /* CRYP_H */
---
+++ cryp.c
@@ -0,0 +1,426 @@
+#include "api/libcryp.h"
+#include "api/regutils.h"
+#include "api/libdma_regs.h"
+#include "api/libdma.h"
+#include "api/syscall.h"
+#include "api/print.h"
+
+static enum crypto_mode current_mode;
+
+void cryp_set_key(const uint8_t * key, enum crypto_key_len key_len)
+{
+    set_reg(r_CORTEX_M_CRYP_CR, key_len, CRYP_CR_KEYSIZE);
+
+    key += (16 + (8 * key_len) - 4);
+    write_reg_value(r_CORTEX_M_CRYP_KxRR(3), to_big32(*(uint32_t *) key));
+    key -= 4;
+    write_reg_value(r_CORTEX_M_CRYP_KxLR(3), to_big32(*(uint32_t *) key));
+    key -= 4;
+    write_reg_value(r_CORTEX_M_CRYP_KxRR(2), to_big32(*(uint32_t *) key));
+    key -= 4;
+    write_reg_value(r_CORTEX_M_CRYP_KxLR(2), to_big32(*(uint32_t *) key));
+    key -= 4;
+
+    if (key_len == CRYP_CR_KEYSIZE_256 || key_len == CRYP_CR_KEYSIZE_192) {
+        write_reg_value(r_CORTEX_M_CRYP_KxRR(1), to_big32(*(uint32_t *) key));
+        key -= 4;
+        write_reg_value(r_CORTEX_M_CRYP_KxLR(1), to_big32(*(uint32_t *) key));
+        key -= 4;
+    }
+
+    if (key_len == CRYP_CR_KEYSIZE_256) {
+        write_reg_value(r_CORTEX_M_CRYP_KxRR(0), to_big32(*(uint32_t *) key));
+        key -= 4;
+        write_reg_value(r_CORTEX_M_CRYP_KxLR(0), to_big32(*(uint32_t *) key));
+        key -= 4;
+    }
+}
+
+void cryp_set_iv(const uint8_t * iv)
+{
+    write_reg_value(r_CORTEX_M_CRYP_IVxLR(0), to_big32(*(uint32_t *) iv));
+    iv += 4;
+    write_reg_value(r_CORTEX_M_CRYP_IVxRR(0), to_big32(*(uint32_t *) iv));
+    iv += 4;
+    write_reg_value(r_CORTEX_M_CRYP_IVxLR(1), to_big32(*(uint32_t *) iv));
+    iv += 4;
+    write_reg_value(r_CORTEX_M_CRYP_IVxRR(1), to_big32(*(uint32_t *) iv));
+    iv += 4;
+}
+
+static void cryp_set_datatype(uint8_t datatype)
+{
+    set_reg(r_CORTEX_M_CRYP_CR, datatype, CRYP_CR_DATATYPE);
+}
+
+static void cryp_set_mode(enum crypto_algo mode)
+{
+    set_reg(r_CORTEX_M_CRYP_CR, mode, CRYP_CR_ALGOMODE);
+}
+
+static void set_dir(enum crypto_dir dir)
+{
+    set_reg(r_CORTEX_M_CRYP_CR, dir, CRYP_CR_ALGODIR);
+}
+
+void enable_crypt(void)
+{
+    set_reg_bits(r_CORTEX_M_CRYP_CR, CRYP_CR_CRYPEN_Msk);
+}
+
+static void disable_crypt(void)
+{
+    clear_reg_bits(r_CORTEX_M_CRYP_CR, CRYP_CR_CRYPEN_Msk);
+}
+
+static int is_busy(void)
+{
+    return get_reg(r_CORTEX_M_CRYP_SR, CRYP_SR_BUSY);
+}
+
+static void flush_fifos(void)
+{
+    set_reg(r_CORTEX_M_CRYP_CR, 1, CRYP_CR_FFLUSH);
+}
+
+static int is_out_fifo_not_empty(void)
+{
+    return get_reg(r_CORTEX_M_CRYP_SR, CRYP_SR_OFNE);
+}
+
+static int is_in_fifo_not_full(void)
+{
+    return get_reg(r_CORTEX_M_CRYP_SR, CRYP_SR_IFNF);
+}
+
+void cryp_disable_dma(void)
+{
+    clear_reg_bits(r_CORTEX_M_CRYP_DMACR, CRYP_DMACR_DIEN_Msk);
+    clear_reg_bits(r_CORTEX_M_CRYP_DMACR, CRYP_DMACR_DOEN_Msk);
+}
+
+
+void cryp_enable_dma(void)
+{
+    set_reg_bits(r_CORTEX_M_CRYP_DMACR, CRYP_DMACR_DIEN_Msk);
+    set_reg_bits(r_CORTEX_M_CRYP_DMACR, CRYP_DMACR_DOEN_Msk);
+}
+
+/*
+** configure, in both CRYP_CFG & CRYP_USER mode. beware to
+** set key to 0 in CRYP_USER mode (or it will lead to memory exception)
+*/
+void cryp_init(const uint8_t * key, enum crypto_key_len key_len,
+               const uint8_t * iv, enum crypto_algo mode, enum crypto_dir dir)
+{
+    disable_crypt();
+
+    // TODO check that the acknowledgement is effective in the buffer
+    // config
+    if (iv) {
+        cryp_set_iv(iv);
+    }
+    if (key) {
+        cryp_set_key(key, key_len);
+    }
+    cryp_set_datatype(CRYP_CR_DATATYPE_BYTES);
+    set_dir(dir);
+
+    /* Prepare key when decryption is asked (except for CTR mode) */
+    if (key && (dir == DECRYPT) && (mode != AES_CTR)) {
+        cryp_set_mode(AES_KEY_PREPARE);
+        enable_crypt();
+        while (is_busy())
+            continue;
+    }
+    cryp_set_mode(mode);
+
+    enable_crypt();
+    flush_fifos();
+}
+
+void cryp_do_no_dma(const uint8_t * data_in, uint8_t * data_out,
+                    uint32_t data_len)
+{
+    uint32_t i, j;
+
+    enable_crypt();
+
+    /* The CRYP FIFO is 8 words deep. Thus we can put
+     * two AES blocks inside it.
+     */
+    i = 0;
+    while (i < data_len / 16) {
+        uint32_t num_states = 1;
+        i++;
+        /* Consume two AES states if we can */
+        if (((data_len / 16) - i) > 1) {
+            num_states = 2;
+            i++;
+        }
+
+        for (j = 0; j < (4 * num_states); j++) {
+            write_reg_value(r_CORTEX_M_CRYP_DIN, *(uint32_t *) data_in);
+            data_in += 4;
+        }
+        while (!is_out_fifo_not_empty())
+            continue;
+        for (j = 0; j < (4 * num_states); j++) {
+            *(uint32_t *) data_out = read_reg_value(r_CORTEX_M_CRYP_DOUT);
+            data_out += 4;
+        }
+
+        while (!is_in_fifo_not_full())
+            continue;
+    }
+
+    while (is_busy())
+        continue;
+}
+
+static dma_t dma_in;
+static dma_t dma_out;
+
+
+void cryp_do_dma(const uint8_t * bufin, const uint8_t * bufout, uint32_t size, int dma_in_desc, int dma_out_desc)
+{
+
+    cryp_enable_dma();
+    e_syscall_ret ret;
+    dma_in.dma          = DMA2;
+    dma_in.stream       = DMA2_STREAM_CRYP_IN;
+    dma_in.channel      = DMA2_CHANNEL_CRYP;
+    dma_in.dir          = MEMORY_TO_PERIPHERAL;
+    dma_in.in_addr      = (physaddr_t) bufin;
+    dma_in.out_addr     = (volatile physaddr_t)r_CORTEX_M_CRYP_DIN;
+    dma_in.in_prio      = DMA_PRI_MEDIUM;
+    dma_in.size         = size;
+    dma_in.mode         = DMA_DIRECT_MODE;
+    dma_in.mem_inc      = 1;
+    dma_in.dev_inc      = 0;
+    dma_in.datasize     = DMA_DS_WORD;
+    dma_in.mem_burst    = INCR4;
+    dma_in.dev_burst    = INCR4;
+    dma_in.flow_control = DMA_FLOWCTRL_DMA;
+    dma_in.in_handler   = (user_dma_handler_t) 0;
+    dma_in.out_handler  = (user_dma_handler_t) 0;    /* not used */
+
+#ifdef CONFIG_USR_DRV_CRYP_DEBUG
+    printf("init DMA CRYP in...\n");
+#endif
+
+    // FIXME - handling ret value
+    ret =
+        sys_cfg(CFG_DMA_RECONF, &dma_in,
+                (DMA_RECONF_BUFIN | DMA_RECONF_BUFOUT | DMA_RECONF_BUFSIZE),
+                dma_in_desc);
+
+#ifdef CONFIG_USR_DRV_CRYP_DEBUG
+    printf("sys_init returns %s !\n", strerror(ret));
+#endif
+    // done by INIT_DONE, by kernel.
+    // enable_crypt_dma(DMA2, DMA2_STREAM_CRYP_IN);
+
+    dma_out.dma         = DMA2;
+    dma_out.stream      = DMA2_STREAM_CRYP_OUT;
+    dma_out.channel     = DMA2_CHANNEL_CRYP;
+    dma_out.dir         = PERIPHERAL_TO_MEMORY;
+    dma_out.in_addr     = (volatile physaddr_t)r_CORTEX_M_CRYP_DOUT;
+    dma_out.out_addr    = (physaddr_t) bufout;
+    dma_out.out_prio    = DMA_PRI_HIGH;
+    dma_out.size        = size;
+    dma_out.mode        = DMA_DIRECT_MODE;
+    dma_out.mem_inc     = 1;
+    dma_out.dev_inc     = 0;
+    dma_out.datasize    = DMA_DS_WORD;
+    dma_out.mem_burst   = INCR4;
+    dma_out.dev_burst   = INCR4;
+    dma_in.flow_control = DMA_FLOWCTRL_DMA;
+    dma_out.in_handler  = (user_dma_handler_t) 0;    /* not used */
+    dma_out.out_handler = (user_dma_handler_t) 0;
+
+#ifdef CONFIG_USR_DRV_CRYP_DEBUG
+    printf("init DMA CRYP out...\n");
+#endif
+
+    // FIXME - handling ret value
+    ret =
+        sys_cfg(CFG_DMA_RECONF, & dma_out,
+                (DMA_RECONF_BUFIN | DMA_RECONF_BUFOUT | DMA_RECONF_BUFSIZE),
+                dma_out_desc);
+#ifdef CONFIG_USR_DRV_CRYP_DEBUG
+    printf("sys_init returns %s !\n", strerror(ret));
+#endif
+}
+
+void cryp_init_dma(void *handler_in, void *handler_out, int dma_in_desc,
+                   int dma_out_desc)
+{
+    e_syscall_ret ret;
+    //enable_crypt();
+    //cryp_enable_dma();
+    cryp_disable_dma();
+
+    dma_in.dma      = DMA2;
+    dma_in.stream   = DMA2_STREAM_CRYP_IN;
+    dma_in.channel  = DMA2_CHANNEL_CRYP;
+    dma_in.dir      = MEMORY_TO_PERIPHERAL;
+    dma_in.in_addr  = (physaddr_t) 0;
+    dma_in.out_addr = (volatile physaddr_t)r_CORTEX_M_CRYP_DIN;
+    dma_in.in_prio  = DMA_PRI_MEDIUM;
+    dma_in.size     = 0;
+    dma_in.mode     = DMA_DIRECT_MODE;
+    dma_in.mem_inc  = 1;
+    dma_in.dev_inc  = 0;
+    dma_in.datasize = DMA_DS_WORD;
+    dma_in.mem_burst    = INCR4;
+    dma_in.dev_burst    = INCR4;
+    dma_in.in_handler   = (user_dma_handler_t) handler_in;
+    dma_in.out_handler  = (user_dma_handler_t) handler_out;  /* not used */
+
+#ifdef CONFIG_USR_DRV_CRYP_DEBUG
+    printf("init DMA CRYP in...\n");
+#endif
+
+    // FIXME - handling ret value
+    ret = sys_cfg(CFG_DMA_RECONF, &dma_in,
+                  (DMA_RECONF_HANDLERS | DMA_RECONF_MODE | DMA_RECONF_PRIO),
+                  dma_in_desc);
+
+#ifdef CONFIG_USR_DRV_CRYP_DEBUG
+    printf("sys_init returns %s !\n", strerror(ret));
+#endif
+    // done by INIT_DONE, by kernel.
+    // enable_crypt_dma(DMA2, DMA2_STREAM_CRYP_IN);
+
+    dma_out.dma         = DMA2;
+    dma_out.stream      = DMA2_STREAM_CRYP_OUT;
+    dma_out.channel     = DMA2_CHANNEL_CRYP;
+    dma_out.dir         = PERIPHERAL_TO_MEMORY;
+    dma_out.in_addr     = (volatile physaddr_t)r_CORTEX_M_CRYP_DOUT;
+    dma_out.out_addr    = (physaddr_t) 0;
+    dma_out.out_prio    = DMA_PRI_HIGH;
+    dma_out.size        = 0;
+    dma_out.mode        = DMA_DIRECT_MODE;
+    dma_out.mem_inc     = 1;
+    dma_out.dev_inc     = 0;
+    dma_out.datasize    = DMA_DS_WORD;
+    dma_out.mem_burst   = INCR4;
+    dma_out.dev_burst   = INCR4;
+    dma_out.in_handler  = (user_dma_handler_t) handler_in;   /* not used */
+    dma_out.out_handler = (user_dma_handler_t) handler_out;
+
+#ifdef CONFIG_USR_DRV_CRYP_DEBUG
+    printf("init DMA CRYP out...\n");
+#endif
+
+    // FIXME - handling ret value
+    ret =
+        sys_cfg(CFG_DMA_RECONF, &dma_out,
+                (DMA_RECONF_HANDLERS | DMA_RECONF_MODE | DMA_RECONF_PRIO),
+                dma_out_desc);
+
+#ifdef CONFIG_USR_DRV_CRYP_DEBUG
+    printf("sys_init returns %s !\n", strerror(ret));
+#endif
+    cryp_enable_dma();
+}
+
+
+void cryp_early_init(bool with_dma,
+                     enum crypto_usage usage,
+                     enum crypto_mode mode,
+                     int *dma_in_desc,
+                     int *dma_out_desc)
+{
+    const char *name = "cryp";
+    e_syscall_ret ret = 0;
+
+    current_mode = mode;
+    device_t dev = { 0 };
+    int      dev_desc = 0;
+    strncpy(dev.name, name, sizeof (dev.name));
+    dev.address = 0x50060000;
+    if (usage == CRYP_USER) {
+        dev.size = 0x100;
+    } else {
+        dev.size = 0x1000;
+    }
+    dev.map_mode = DEV_MAP_AUTO;
+    dev.irq_num = 0;
+    dev.gpio_num = 0;
+
+#ifdef CONFIG_USR_DRV_CRYP_DEBUG
+    printf("registering cryp-user driver\n");
+#endif
+
+    // FIXME - proper handling of ret
+    ret = sys_init(INIT_DEVACCESS, &dev, &dev_desc);
+
+#ifdef CONFIG_USR_DRV_CRYP_DEBUG
+    printf("sys_init returns %s !\n", strerror(ret));
+#endif
+
+    if (!with_dma) {
+      goto end;
+    }
+    dma_in.channel = DMA2_CHANNEL_CRYP;
+    dma_in.dir = MEMORY_TO_PERIPHERAL;
+    dma_in.in_addr = (physaddr_t) 0;
+    dma_in.out_addr = (volatile physaddr_t)r_CORTEX_M_CRYP_DIN;
+    dma_in.in_prio = DMA_PRI_MEDIUM;
+    dma_in.dma = DMA2;
+    dma_in.size = 0;
+    dma_in.stream = DMA2_STREAM_CRYP_IN;
+    dma_in.mode = DMA_DIRECT_MODE;
+    dma_in.mem_inc = 1;
+    dma_in.dev_inc = 0;
+    dma_in.datasize = DMA_DS_WORD;
+    dma_in.mem_burst = INCR4;
+    dma_in.dev_burst = INCR4;
+    dma_in.in_handler = (user_dma_handler_t) 0;
+    dma_in.out_handler = (user_dma_handler_t) 0;    /* not used */
+
+#ifdef CONFIG_USR_DRV_CRYP_DEBUG
+    printf("init DMA CRYP in...\n");
+#endif
+
+    // FIXME - proper handling of ret and of dma_in_desc (if < 0)
+    ret = sys_init(INIT_DMA, &dma_in, dma_in_desc);
+
+#ifdef CONFIG_USR_DRV_CRYP_DEBUG
+    printf("sys_init returns %s !\n", strerror(ret));
+#endif
+    // done by INIT_DONE, by kernel.
+    // enable_crypt_dma(DMA2, DMA2_STREAM_CRYP_IN);
+
+    dma_out.channel = DMA2_CHANNEL_CRYP;
+    dma_out.dir = PERIPHERAL_TO_MEMORY;
+    dma_out.in_addr = (volatile physaddr_t)r_CORTEX_M_CRYP_DOUT;
+    dma_out.out_addr = (physaddr_t) 0;
+    dma_out.out_prio = DMA_PRI_HIGH;
+    dma_out.dma = DMA2;
+    dma_out.size = 0;
+    dma_out.stream = DMA2_STREAM_CRYP_OUT;
+    dma_out.mode = DMA_DIRECT_MODE;
+    dma_out.mem_inc = 1;
+    dma_out.dev_inc = 0;
+    dma_out.datasize = DMA_DS_WORD;
+    dma_out.mem_burst = INCR4;
+    dma_out.dev_burst = INCR4;
+    dma_out.in_handler = (user_dma_handler_t) 0;    /* not used */
+    dma_out.out_handler = (user_dma_handler_t) 0;
+
+#ifdef CONFIG_USR_DRV_CRYP_DEBUG
+    printf("init DMA CRYP out...\n");
+#endif
+
+    // FIXME - proper handling of ret and of dma_out_desc (if < 0)
+    ret = sys_init(INIT_DMA, &dma_out, dma_out_desc);
+
+#ifdef CONFIG_USR_DRV_CRYP_DEBUG
+    printf("sys_init returns %s !\n", strerror(ret));
+#endif
+end:
+    return;
+}
