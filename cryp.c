f4efdafce266a8a4c8617b967921acc5850b0131
Philippe Thierry <phil@reseau-libre.net>
2019-03-18 10:07:23 +0100

    [update] Updated libstd usage, including stdio.h/nostd.h headers instead of print.h

--- cryp.c
+++ cryp.c
@@ -1,7 +1,8 @@
 #include "api/libcryp.h"
 #include "api/regutils.h"
 #include "api/syscall.h"
-#include "api/print.h"
+#include "api/stdio.h"
+#include "api/nostd.h"
 #include "api/string.h"
 
 enum dma_controller {
