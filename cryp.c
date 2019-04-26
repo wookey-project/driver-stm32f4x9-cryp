2237aa6aa556120e9c45be94942c3faa386a43a3
Ryad Benadjila <ryadbenadjila@gmail.com>
2019-04-26 16:31:00 +0200

    [CodeSonar] Fix missing const qualifier in cast.

--- cryp.c
+++ cryp.c
@@ -91,14 +91,14 @@ void cryp_set_iv(const uint8_t * iv, unsigned int iv_len)
     if((iv_len != 8) && (iv_len != 16)){
         return;
     }
-    write_reg_value(r_CORTEX_M_CRYP_IVxLR(0), htonl(*(uint32_t *) iv));
+    write_reg_value(r_CORTEX_M_CRYP_IVxLR(0), htonl(*(const uint32_t *) iv));
     iv += 4;
-    write_reg_value(r_CORTEX_M_CRYP_IVxRR(0), htonl(*(uint32_t *) iv));
+    write_reg_value(r_CORTEX_M_CRYP_IVxRR(0), htonl(*(const uint32_t *) iv));
     if(iv_len == 16){
         iv += 4;
-        write_reg_value(r_CORTEX_M_CRYP_IVxLR(1), htonl(*(uint32_t *) iv));
+        write_reg_value(r_CORTEX_M_CRYP_IVxLR(1), htonl(*(const uint32_t *) iv));
         iv += 4;
-        write_reg_value(r_CORTEX_M_CRYP_IVxRR(1), htonl(*(uint32_t *) iv));
+        write_reg_value(r_CORTEX_M_CRYP_IVxRR(1), htonl(*(const uint32_t *) iv));
         iv += 4;
     }
 }
@@ -212,26 +212,26 @@ void cryp_set_key(const uint8_t * key, enum crypto_key_len key_len)
     set_reg(r_CORTEX_M_CRYP_CR, key_len, CRYP_CR_KEYSIZE);
 
     key += (16 + (8 * key_len) - 4);
-    write_reg_value(r_CORTEX_M_CRYP_KxRR(3), htonl(*(uint32_t *) key));
+    write_reg_value(r_CORTEX_M_CRYP_KxRR(3), htonl(*(const uint32_t *) key));
     key -= 4;
-    write_reg_value(r_CORTEX_M_CRYP_KxLR(3), htonl(*(uint32_t *) key));
+    write_reg_value(r_CORTEX_M_CRYP_KxLR(3), htonl(*(const uint32_t *) key));
     key -= 4;
-    write_reg_value(r_CORTEX_M_CRYP_KxRR(2), htonl(*(uint32_t *) key));
+    write_reg_value(r_CORTEX_M_CRYP_KxRR(2), htonl(*(const uint32_t *) key));
     key -= 4;
-    write_reg_value(r_CORTEX_M_CRYP_KxLR(2), htonl(*(uint32_t *) key));
+    write_reg_value(r_CORTEX_M_CRYP_KxLR(2), htonl(*(const uint32_t *) key));
     key -= 4;
 
     if (key_len == CRYP_CR_KEYSIZE_256 || key_len == CRYP_CR_KEYSIZE_192) {
-        write_reg_value(r_CORTEX_M_CRYP_KxRR(1), htonl(*(uint32_t *) key));
+        write_reg_value(r_CORTEX_M_CRYP_KxRR(1), htonl(*(const uint32_t *) key));
         key -= 4;
-        write_reg_value(r_CORTEX_M_CRYP_KxLR(1), htonl(*(uint32_t *) key));
+        write_reg_value(r_CORTEX_M_CRYP_KxLR(1), htonl(*(const uint32_t *) key));
         key -= 4;
     }
 
     if (key_len == CRYP_CR_KEYSIZE_256) {
-        write_reg_value(r_CORTEX_M_CRYP_KxRR(0), htonl(*(uint32_t *) key));
+        write_reg_value(r_CORTEX_M_CRYP_KxRR(0), htonl(*(const uint32_t *) key));
         key -= 4;
-        write_reg_value(r_CORTEX_M_CRYP_KxLR(0), htonl(*(uint32_t *) key));
+        write_reg_value(r_CORTEX_M_CRYP_KxLR(0), htonl(*(const uint32_t *) key));
         key -= 4;
     }
     while (is_busy()){
